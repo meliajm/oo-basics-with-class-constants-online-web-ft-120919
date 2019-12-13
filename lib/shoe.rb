86ec3c0b6343aeb0740669a79fd27c1dc098f05b
melia miller <meliajm@gmail.com>
2019-12-13 01:38:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,6 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   BRANDS = []
+  
   def initialize(brand)
     @brand = brand
     BRANDS << brand if !BRANDS.include?(brand)
@@ -11,7 +12,5 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
-  
   
 end
